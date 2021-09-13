e1fd46a43dcae8a10b1a0f4ce3b2dfb1e565acfe
immanuveljeeva08 <immanuveljeeva2000@gmail.com>
2021-09-13 09:39:42 -0700

    To display Hello from Bridgelabz Using STS

---
+++ SpringdemoAppWithSTS/src/test/java/com/example/springdemoappwithsts/SpringdemoAppWithStsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdemoappwithsts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoAppWithStsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
