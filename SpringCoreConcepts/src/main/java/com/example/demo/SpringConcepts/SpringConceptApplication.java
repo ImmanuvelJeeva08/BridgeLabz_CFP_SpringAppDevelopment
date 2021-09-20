3528cc7bf215ddffb6adf5f48ae4745affabe2bf
immanuveljeeva08 <immanuveljeeva2000@gmail.com>
2021-09-20 11:09:32 -0700

    Ability to add Logger to Log Messages

--- SpringCoreConcepts/src/main/java/com/example/demo/SpringConcepts/SpringConceptApplication.java
+++ SpringCoreConcepts/src/main/java/com/example/demo/SpringConcepts/SpringConceptApplication.java
@@ -1,13 +1,36 @@
+/*************************************************************************************************************
+ *
+ * UC2 : Add Logger to Log Messages
+ * 	   - Use Built In SLF4J
+ * 	   – Simple Logging Framework
+ *
+ * @author : IMMANUVEL JEEVA
+ * @since  : 13.09.2021
+ */
 package com.example.demo.SpringConcepts;
 
+import com.example.demo.SpringConcepts.component.DemoBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringConceptApplication {
+    public static final Logger logger = LoggerFactory.getLogger(SpringConceptApplication.class);
 
     public static void main(String[] args) {
 
-        SpringApplication.run(SpringConceptApplication.class, args);
+        /**
+         * Purpose : Printing the logging message based on the pattern provided in logback.xml
+         * Since logger.debug is mentioned, so it will display the logging message from and above
+         * the DEBUG level.
+         */
+
+        logger.debug("Welcome to Spring Concept Demo");
+        ApplicationContext context = SpringApplication.run(SpringConceptApplication.class, args);
+        logger.debug("Checking Context: {}", context.getBean(DemoBean.class));
+        logger.debug("\n*** Example Using @Autowire annotation on property ***");
     }
 }
